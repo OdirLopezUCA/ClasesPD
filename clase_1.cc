118104848f75c8774659758da204eb2cad3a6cc8
Odir <00077325@uca.edu.sv>
2025-08-19 12:39:09 -0600

    Clase 1 a medias

---
+++ clase_1.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+std::string GenerarCupon(std::string prefijo);
+void VerificarCupon (std::string cupon);
+void VerificarLetras(std::string letras);
+
+int main()
+{
+    srand(time(0));
+    // Solicitar al usuario las letras del cupon
+
+    // Validar si tiene 3 letras o no length()
+
+    // Llamar a la funcion generar cupon
+    std::string cupon_generado = GenerarCupon("AGO");
+    std::cout << "El cupon generado es: " << cupon_generado;
+    VerificarCupon(cupon_generado);
+    return 0;
+}
+
+std::string GenerarCupon(std::string prefijo)
+{
+    // Generar el numero aleatrorio
+    int randomNum = rand() % 100 + 999;
+
+    // Convertir entero a string
+    std::string conversion = std::to_string(randomNum);
+
+    // Concatenar el prefijo y el numero aleatorio
+
+    return prefijo + conversion;
+}
+
+void VerificarCupon (std::string cupon){
+    //Extraer la parte numerica del cupon
+    std::string num_extraido = cupon.substr(3, 4);
+
+    //Convertir de entero a string
+    int num_conversion = std::stoi(num_extraido);
+
+    //Validar si es par o no es par
+    if (num_conversion % 2)
+    {
+        std::cout << "Es numero par";
+    } else
+    {
+        std::cout << "No es par";
+    }
+    
+    
+}
