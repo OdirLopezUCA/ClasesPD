719c4847a1f931eb03b0d3ca8afffea8d4e4af4f
Odir <00077325@uca.edu.sv>
2025-08-21 12:33:17 -0600

    codigo mas actualizado

--- clase_1.cc
+++ clase_1.cc
@@ -1,63 +1,92 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <vector> 
 
+// Declaración de las funciones
+std::string solicitarDatos();
 std::string GenerarCupon(std::string prefijo);
-void VerificarCupon (std::string cupon);
-void VerificarLetras(std::string letras);
+void VerificarCupon(std::string cupon);
+
 
 int main()
 {
     srand(time(0));
+
     int cantidad_cupones = 0;
-    std::string cupones_generados[10];
-    //Solicitar la cantidad de cupones a generar
-    std::cout<<"Ingrese la cantidad de cupones a generar: "<<endl;
-    std::cin>>cantidad_cupones;
-    //almacenar en un arreglo
-    for (int i = 0; i < cantidad_cupones; i++){
 
-        cupones_generados[i] = GenerarCupon(SolicitarDatos());
+    // Solicitar la cantidad de cupones a generar
+    std::cout << "Ingrese la cantidad de cupones a generar: ";
+    std::cin >> cantidad_cupones;
+
+    // Se declara el vector después de obtener la cantidad para darle el tamaño correcto.
+    std::vector<std::string> cupones_generados(cantidad_cupones);
+
+    // Almacear en un vector
+    for (int i = 0; i < cantidad_cupones; i++)
+    {
+        // Llamar a la funcion generar cupon y solicitar datos
+        cupones_generados[i] = GenerarCupon(solicitarDatos());
     }
-    // Solicitar al usuario las letras del cupon
 
-    // Validar si tiene 3 letras o no length()
+    // Mostrar los cupones generados y verificar cada uno
+    for (int i = 0; i < cantidad_cupones; i++)
+    {
+        std::cout << "El cupon generado es: " << cupones_generados[i] << std::endl;
+        VerificarCupon(cupones_generados[i]);
+    }
 
-    // Llamar a la funcion generar cupon
-    std::string cupones_generados = GenerarCupon("AGO");
-    std::cout << "El cupon generado es: " << cupon_generado;
-    VerificarCupon(cupon_generado);
     return 0;
 }
 
+// Definicion de la funcion solicitarDatos
+std::string solicitarDatos()
+{
+    std::string prefijo;
+    do
+    {
+        std::cout << "Ingrese las 3 letras del cupon: ";
+        std::cin >> prefijo;
+
+        if (prefijo.length() != 3)
+        {
+            std::cout << "Error, deben ser 3 letras." << std::endl;
+        }
+    } while (prefijo.length() != 3);
+
+    return prefijo;
+}
+
+// Definicion de la funcion GenerarCupon
 std::string GenerarCupon(std::string prefijo)
 {
-    // Generar el numero aleatrorio
-    int randomNum = rand() % 100 + 999;
+    // Generar el aleatorio de 3 dígitos (100 a 999)
+    int num_aleatorio = 100 + rand() % 900; 
 
     // Convertir entero a string
-    std::string conversion = std::to_string(randomNum);
+    std::string conversion = std::to_string(num_aleatorio);
 
-    // Concatenar el prefijo y el numero aleatorio
-
+    // Concatenar el prefijo con el aleatorio
     return prefijo + conversion;
 }
 
-void VerificarCupon (std::string cupon){
-    //Extraer la parte numerica del cupon
-    std::string num_extraido = cupon.substr(3, 4);
+// Definicion de la funcion VerificarCupon
+void VerificarCupon(std::string cupon)
+{
+    // Extraer parte numerica del cupon, los 3 dígitos finales
+    std::string num_extraido = cupon.substr(3, 3);
 
-    //Convertir de entero a string
-    int num_conversion = std::stoi(num_extraido);
+    // Convertir de string a entero
+    int num_cupon = std::stoi(num_extraido);
 
-    //Validar si es par o no es par
-    if (num_conversion % 2)
+    // Validar si es par o no es par
+    if (num_cupon % 2 == 0)
     {
-        std::cout << "Es numero par";
-    } else
-    {
-        std::cout << "No es par";
+        std::cout << "Tiene premio." << std::endl;
     }
-    
-    
+    else
+    {
+        std::cout << "No tiene premio." << std::endl;
+    }
 }
